e62660a38c273a4c76043b75e4899245e286ab34
Ryan Milne <ryanmilne101@gmail.com>
2020-08-05 13:44:01 -0600

    done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const massive = require("massive");
 const app = express();
+const ctrl = require("./products_controller");
 
 const { SERVER_PORT, CONNECTION_STRING } = process.env;
 
@@ -15,6 +16,12 @@ massive({
   .catch((err) => console.log(err));
 
 app.use(express.json());
+
+app.post("/api/products", ctrl.create);
+app.get("/api/products", ctrl.getAll);
+app.get("/api/products/:id", ctrl.getOne);
+app.put("/api/products/:id", ctrl.update);
+app.delete("/api/products/:id", ctrl.delete);
 
 app.listen(SERVER_PORT, () => {
   console.log(`The Server is listening ${SERVER_PORT}`);
